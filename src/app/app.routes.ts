1c9a66e8f5c06b710d0f53231599024d9125cbd2
JefJim <jefryjimenez2011@gmail.com>
2025-11-10 16:24:00 -0600

    feat: update routing and styles for two-factor authentication and Angular Material theming

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
+import { Dashboard } from './pages/dashboard/dashboard';
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
 import { Register } from './auth/register/register';
+import { AuthGuard } from './guards/auth.guard';
+import { TwoFactorSetup } from './auth/twofa-setup/twofa-setup';
+import { TwoFactorVerify } from './auth/twofa-verify/twofa-verify';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   { path: 'auth/login', component: Login },
-  { path: 'auth/register', component: Register }
+  { path: 'auth/register', component: Register },
+  { path: 'pages/dashboard', component: Dashboard, canActivate: [AuthGuard] },
+  { path: 'auth/2fa/setup', component: TwoFactorSetup },
+  { path: 'auth/2fa/verify', component: TwoFactorVerify },
 ];
