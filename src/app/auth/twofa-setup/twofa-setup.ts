14dead825468119c1bfd581ca1cd21027e5bf8b9
JefJim <jefryjimenez2011@gmail.com>
2025-11-12 15:43:05 -0600

    Fixing 2fa setup, loading QR code properly

--- src/app/auth/twofa-setup/twofa-setup.ts
+++ src/app/auth/twofa-setup/twofa-setup.ts
@@ -13,12 +13,12 @@ import { CommonModule } from '@angular/common';
 export class TwoFactorSetup implements OnInit {
   form!: FormGroup;
   qrCodeDataUrl: string | null = null;
-
+  qrSecret: string | null = null;
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
     private snackBar: MatSnackBar
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -29,23 +29,39 @@ export class TwoFactorSetup implements OnInit {
   }
 
   loadQrCode(): void {
-    this.http.get<{ qrCodeDataURL: string }>('http://localhost:5000/api/2fa/setup')
+    this.http.get<{ qrImage: string , secret: string}>('http://localhost:5000/api/twofactor/setup')
       .subscribe({
-        next: (res) => this.qrCodeDataUrl = res.qrCodeDataURL,
+        next: (res) => {this.qrCodeDataUrl = res.qrImage; 
+        this.qrSecret = res.secret;},
+        
+        
         error: () => this.snackBar.open('Error al cargar el código QR', 'Cerrar', { duration: 3000 }),
       });
   }
 
   onSubmit(): void {
-    if (this.form.invalid) return;
-
-    this.http.post('http://localhost:5000/api/2fa/activate', this.form.value)
-      .subscribe({
-        next: () => {
-          this.snackBar.open('✅ 2FA activado correctamente', 'Cerrar', { duration: 3000 });
-          setTimeout(() => window.location.href = '/auth/login', 3000);
-        },
-        error: () => this.snackBar.open('Código inválido', 'Cerrar', { duration: 3000 }),
-      });
+  if (this.form.invalid || !this.qrSecret) {
+    this.snackBar.open('Falta el código secreto o el formulario es inválido', 'Cerrar', { duration: 3000 });
+    return;
   }
+
+  const body = {
+    secret: this.qrSecret,             
+    code: this.form.value.token         
+  };
+
+  this.http.post('http://localhost:5000/api/twofactor/enable', body)
+    .subscribe({
+      next: () => {
+        this.snackBar.open('✅ 2FA activado correctamente', 'Cerrar', { duration: 3000 });
+        setTimeout(() => window.location.href = '/auth/dashboard', 3000);
+      },
+      error: (err) => {
+        console.error(err);
+        this.snackBar.open(err.error?.message || 'Código inválido', 'Cerrar', { duration: 3000 });
+      },
+    });
+}
+
+
 }
