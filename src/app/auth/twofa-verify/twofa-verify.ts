d6fedf874a70ce17ff3a3af38ec4cf646da7a64f
JefJim <jefryjimenez2011@gmail.com>
2025-11-12 15:44:02 -0600

    Adding token validation every time that the user log in in the app

--- src/app/auth/twofa-verify/twofa-verify.ts
+++ src/app/auth/twofa-verify/twofa-verify.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -18,20 +18,37 @@ export class TwoFactorVerify {
     private snackBar: MatSnackBar
   ) {
     this.form = this.fb.group({
-      token: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
+      Code: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]],
     });
   }
 
   onSubmit(): void {
     if (this.form.invalid) return;
 
-    this.http.post('http://localhost:5000/api/2fa/verify', this.form.value)
+    const token = localStorage.getItem('token'); // JWT parcial del login
+    if (!token) {
+      this.snackBar.open('No hay sesión activa', 'Cerrar', { duration: 3000 });
+      return;
+    }
+    const body = { Code: this.form.value.Code , Secret: ''}; // Añadir el campo Secret vacío  
+    this.http.post('http://localhost:5000/api/twofactor/verify', body, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
       .subscribe({
-        next: () => {
+        next: (res: any) => {
+          localStorage.setItem('token', res.token);
           this.snackBar.open('✅ Verificación correcta', 'Cerrar', { duration: 3000 });
-          setTimeout(() => window.location.href = '/dashboard', 3000);
+          setTimeout(() => window.location.href = 'pages/dashboard', 3000);
         },
-        error: () => this.snackBar.open('Código incorrecto o expirado', 'Cerrar', { duration: 3000 }),
+        error: (err) => {
+          console.error(err);
+          
+          this.snackBar.open(err.error?.message || 'Código incorrecto o expirado', 'Cerrar', { duration: 3000 });
+        },
       });
   }
+
 }
